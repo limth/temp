cf41defdfa1bf8f6a9fab48bd9f65a6f7df4b864
limth <radomir.szewczyk@gmail.com>
2013-09-09 23:42:04 +0200

    Another test.

--- quest-calculator/quest-calculator.js
+++ quest-calculator/quest-calculator.js
@@ -1,3 +1,10 @@
 questCalculator = {
     
 }
+
+$(document).ready(function () {
+    $('#test').text('TEST');
+    $('#test').click(function (data) {
+        $(data.target).text($(data.target).text() + "1");  
+    });
+});
