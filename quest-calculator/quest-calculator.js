ba4a6dd85fec159e885c31d40f958d87cdce3f4f
limth <radomir.szewczyk@gmail.com>
2013-09-20 14:02:48 +0200

    Added option mechanism.

--- quest-calculator/quest-calculator.js
+++ quest-calculator/quest-calculator.js
@@ -1,10 +1,43 @@
 questCalculator = {
-    
+    selectedOption: 0,
+    questWidth: 80,
+    questHeight: 124,
+
+    initialize: function () {
+        this.selectedOption = $('input[name="calc-option"][checked]').val();
+        this.generateInputDiv(this.selectedOption);
+        $('input[name="calc-option"]').change(function (data) {
+            $target = $(data.target);
+            this.selectedOption = $target.val();
+            questCalculator.generateInputDiv(this.selectedOption);
+        });
+    },
+    generateInputDiv: function (option) {
+        $inputDiv = $('<div>').attr('id', 'inputWrapper');
+        $('#calc-render').html('')
+            .append('<br/>')
+            .append($inputDiv);
+        if (option == '1') {
+            $('#inputWrapper').append(this.generateFirstOption);        
+        } else if (option == '2') {
+            $('#inputWrapper').append(this.generateSecondOption);
+        }
+    },
+    generateFirstOption: function () {
+        return '<span>Rozmiar ściany: szerokość </span>'
+            + '<input type="text" name="widthInput" />'
+            + '<span>[cm] x wysokość </span>'
+            + '<input type="text" name="heightInput" />'
+            + '<span>[cm].</span>';
+    },
+    generateSecondOption: function () {
+        return 'Ściana Questów: liczba rzędów '
+            + '<input type="text" name="widthInput" />'
+            + ' x liczba kolumn '
+            + '<input type="text" name="heightInput" />.'
+    },
 }
 
 $(document).ready(function () {
-    $('#test').text('TEST');
-    $('#test').click(function (data) {
-        $(data.target).text($(data.target).text() + "1");  
-    });
+    questCalculator.initialize();
 });
