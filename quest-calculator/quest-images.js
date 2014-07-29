fe429966a34151ce204fc06ea526454509c36fd1
szewczykr <r.szewczyk@tt.com.pl>
2014-07-29 19:38:30 +0200

    Changed directories for images to match destination site.

--- quest-calculator/quest-images.js
+++ quest-calculator/quest-images.js
@@ -4,15 +4,15 @@ questImages = {
     assemblyImage: null,
     baseImage: null,
     setHqImageSet: function () {
-        this.topImage = '<img src="quest-img/quest_up_2.png" alt="quest-top-img" />';
-        this.bottomImage = '<img src="quest-img/quest_down_2.png" alt="quest-bottom-img" />';
-        this.assemblyImage = '<img src="quest-img/quest_down_assembly_2.png" alt="quest-assembly-img" />';
-        this.baseImage = '<img src="quest-img/quest_base_2.png" alt="quest-base-img" />';
+        this.topImage = '<img src="img/quest-img/quest_up_2.png" alt="quest-top-img" />';
+        this.bottomImage = '<img src="img/quest-img/quest_down_2.png" alt="quest-bottom-img" />';
+        this.assemblyImage = '<img src="img/quest-img/quest_down_assembly_2.png" alt="quest-assembly-img" />';
+        this.baseImage = '<img src="img/quest-img/quest_base_2.png" alt="quest-base-img" />';
     },
     setCompressedImageSet: function () {
-        this.topImage = '<img src="quest-img-compressed/quest_up_2.png" alt="quest-top-img" />';
-        this.bottomImage = '<img src="quest-img-compressed/quest_down_2.png" alt="quest-bottom-img" />';
-        this.assemblyImage = '<img src="quest-img-compressed/quest_down_assembly_2.png" alt="quest-assembly-img" />';
-        this.baseImage = '<img src="quest-img-compressed/quest_base_2.png" alt="quest-base-img" />';
+        this.topImage = '<img src="img/quest-img-compressed/quest_up_2.png" alt="quest-top-img" />';
+        this.bottomImage = '<img src="img/quest-img-compressed/quest_down_2.png" alt="quest-bottom-img" />';
+        this.assemblyImage = '<img src="img/quest-img-compressed/quest_down_assembly_2.png" alt="quest-assembly-img" />';
+        this.baseImage = '<img src="img/quest-img-compressed/quest_base_2.png" alt="quest-base-img" />';
     }
 }
